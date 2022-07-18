a8aebcd72f2fe94957df0bce5e067652b1317bc0
antnn <15718969+antnn@users.noreply.github.com>
2022-07-18 19:25:07 +0500

    Update portal.py

--- portal.py
+++ portal.py
@@ -1,3 +1,4 @@
+import sys
 from random import randrange
 from gio import *
 
@@ -51,16 +52,62 @@ def setup_request_response_signal(object_path: str,
                                               callback, user_data, None)
 
 
-
 @CFUNCTYPE(c_int, POINTER(c_int), POINTER(c_int))
 def py_cmp_func(a, b):
     print("py_cmp_func", a[0], b[0])
     return 1
 
 
+@CFUNCTYPE(None, GDBusConnection_p, c_char_p, c_char_p, c_char_p, c_char_p, GVariant_p, c_void_p)
+def on_session_closed_signal(connection: GDBusConnection_p,
+                             sender_name: c_char_p,
+                             object_path: c_char_p,
+                             interface_name: c_char_p,
+                             signal_name: c_char_p,
+                             parameters: GVariant_p,
+                             user_data: c_void_p):
+    pass
 
-@CFUNCTYPE(c_int, GDBusConnection_p, c_char_p, c_char_p, c_char_p, c_char_p, GVariant_p, c_void_p)
-def request_session_response_signal_handler(
+
+kSessionInterfaceName = "org.freedesktop.portal.Session"
+
+
+class CaptureSourceType:
+    kScreen = 0b01
+    kWindow = 0b10
+    kAnyScreenContent = kScreen | kWindow
+
+
+class CursorMode:
+    """Values are set based on cursor mode property in
+    xdg-desktop-portal/screencast
+    https://github.com/flatpak/xdg-desktop-portal/blob/master/data/org.freedesktop.portal.ScreenCast.xml"""
+    kHidden = 0b01, """Mouse cursor will not be included in any form"""
+    """Mouse cursor will be part of the screen content"""
+    kEmbedded = 0b10
+    """Mouse cursor information will be send separately in form of metadata"""
+    kMetadata = 0b100
+
+
+class PersistMode:
+    """Values are set based on persist mode property in
+    xdg-desktop-portal/screencast
+    https://github.com/flatpak/xdg-desktop-portal/blob/master/data/org.freedesktop.portal.ScreenCast.xml"""
+    kDoNotPersist = 0b00, """ Do not allow to restore stream """
+    """The restore token is valid as long as the application is alive. It's
+       stored in memory and revoked when the application closes its DBus
+       connection"""
+    kTransient = 0b01
+    """The restore token is stored in disk and is valid until the user manually
+       revokes it"""
+    kPersistent = 0b10
+
+
+cursor_mode_ = CursorMode.kMetadata
+restore_token_ = ""
+
+@CFUNCTYPE(None, GDBusConnection_p, c_char_p, c_char_p, c_char_p, c_char_p, GVariant_p, c_void_p)
+def start_request_response_signal_handler(
         connection: GDBusConnection_p,
         sender_name: c_char_p,
         object_path: c_char_p,
@@ -69,17 +116,123 @@ def request_session_response_signal_handler(
         parameters: GVariant_p,
         user_data: c_void_p):
     print("___________CALLBACK__________ works")
-    portal_response = 0
-    #response_data: POINTER(GVariant_p)()
-    return 1
-    # g_variant_get(parameters, "(u@a{sv})", POINTER(portal_response), response_data)
+
+
+def start_request():
+    global session_handle_
+    builder = g_variant_builder_new(G_VARIANT_TYPE_VARDICT)
+    #token for handle
+    variant_string = "%s_%d" % (portal_prefix, randrange(0, PLATFORM_C_MAXINT))
+    g_variant_builder_add(builder, "{sv}", "handle_token", g_variant_new_string(variant_string))
+    start_handle = prepare_signal_handle(variant_string, connection_)
+    start_request_signal_id = setup_request_response_signal(
+        start_handle, start_request_response_signal_handler, None, connection_)
+    parent_window = ""
+    print("Starting the portal session.\n")
+    res = g_dbus_proxy_call_sync(
+        screencast_proxy_, "Start",
+        g_variant_new("(osa{sv})", session_handle_, parent_window, builder),
+        G_DBUS_CALL_FLAGS_NONE, -1, cancellable_)
+    if not res:
+        print("Error", file=sys.stderr)
+        exit(-1)
+
+def on_portal_done():
+    pass
+
+
+@CFUNCTYPE(None, GDBusConnection_p, c_char_p, c_char_p, c_char_p, c_char_p, GVariant_p, c_void_p)
+def sources_request_response_signal_handler(
+        connection: GDBusConnection_p,
+        sender_name: c_char_p,
+        object_path: c_char_p,
+        interface_name: c_char_p,
+        signal_name: c_char_p,
+        parameters: GVariant_p,
+        user_data: c_void_p):
+    portal_response = c_uint32(0)
+    g_variant_get(parameters, "(u@a{sv})", byref(portal_response), None);
+    if portal_response:
+        print("Failed to select sources for the screen cast session.", file=sys.stderr)
+        on_portal_done()
+    start_request()
+
+
+def sources_request(session_handle_):
+    builder = g_variant_builder_new(G_VARIANT_TYPE_VARDICT)
+    token_string = ""
+    g_variant_builder_add(builder, "{sv}", "types", g_variant_new_uint32(CaptureSourceType.kScreen))
+    g_variant_builder_add(builder, "{sv}", "multiple", g_variant_new_boolean(False))
+    cursor_modes_variant = g_dbus_proxy_get_cached_property(screencast_proxy_, "AvailableCursorModes")
+    if cursor_modes_variant:
+        modes = c_uint32(0)
+        g_variant_get(cursor_modes_variant, "u", byref(modes))
+        modes = modes.value
+        if modes & cursor_mode_:
+            g_variant_builder_add(builder, "{sv}", "cursor_mode",
+                                  g_variant_new_uint32(cursor_mode_))
+    versionVariant = g_dbus_proxy_get_cached_property(screencast_proxy_, "version")
+    if versionVariant:
+        version = c_uint32(0)
+        g_variant_get(versionVariant, "u", byref(version))
+        version = version.value
+        if version >= 4:
+            persist_mode_ = PersistMode.kTransient
+            g_variant_builder_add(builder, "{sv}", "persist_mode", g_variant_new_uint32(persist_mode_))
+            if len(restore_token_):
+                g_variant_builder_add(builder, "{sv}", "restore_token",
+                                      g_variant_new_string(restore_token_))
+
+    token_string = "%s_%d" % (portal_prefix, randrange(0, PLATFORM_C_MAXINT))
+    g_variant_builder_add(builder, "{sv}", "handle_token",
+                          g_variant_new_string(token_string))
+    sources_handle = prepare_signal_handle(token_string, connection_)
+    sources_request_signal_id_ = setup_request_response_signal(
+        sources_handle, sources_request_response_signal_handler,
+        None, connection_)
+    print("Requesting sources from the screen cast session.\n")
+    result = g_dbus_proxy_call_sync(
+        screencast_proxy_, "SelectSources",
+        g_variant_new("(oa{sv})", session_handle_, builder),
+        G_DBUS_CALL_FLAGS_NONE, -1, cancellable_)
+    if not result:
+        exit(-1)
+
+global session_handle_
+
+@CFUNCTYPE(None, GDBusConnection_p, c_char_p, c_char_p, c_char_p, c_char_p, GVariant_p, c_void_p)
+def request_session_response_signal_handler(
+        connection: GDBusConnection_p,
+        sender_name: c_char_p,
+        object_path: c_char_p,
+        interface_name: c_char_p,
+        signal_name: c_char_p,
+        parameters: GVariant_p,
+        user_data: c_void_p):
+    portal_response = c_uint32(1)
+    response_data = c_void_p(0)
+    g_variant_get(c_void_p(parameters), "(u@a{sv})", byref(portal_response), byref(response_data))
+    g_session_handle = g_variant_lookup_value(response_data, "session_handle", None)
+    global session_handle_
+    session_handle_ = g_variant_dup_string(g_session_handle, None)
+    if session_handle_ == "" or not session_handle_ or portal_response:
+        print("Failed to request the session subscription.\n", file=sys.stderr)
+        # OnPortalDone(RequestResponse::kError);
+        return
+    global session_request_signal_id_
+    session_closed_signal_id_ = g_dbus_connection_signal_subscribe(
+        connection, kDesktopBusName, kSessionInterfaceName, "Closed",
+        session_handle_, None, G_DBUS_SIGNAL_FLAGS_NONE,
+        on_session_closed_signal, None, None)
+    sources_request(session_handle_)
 
 
 session_request_signal_id_ = 0
 cancellable_ = g_cancellable_new()
 
+
 def setup_session_request_handlers(connection_):
-    #global connection_
+    # global connection_
     global session_request_signal_id_
     builder = g_variant_builder_new(G_VARIANT_TYPE_VARDICT)
     session_handle_token = "%s_session%d" % (portal_prefix, randrange(0, PLATFORM_C_MAXINT))
@@ -95,7 +248,9 @@ def setup_session_request_handlers(connection_):
     variant = g_variant_new("(a{sv})", builder)
     res = g_dbus_proxy_call_sync(screencast_proxy_, "CreateSession", variant,
                                  G_DBUS_CALL_FLAGS_NONE, -1, cancellable_)
-    print("Result %s", res)
+    if res.error():
+        print("Error during call to *CreateSession*\n", file=sys.stderr)
+        exit(-1)
 
 
 def portal():
